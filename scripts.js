8c10679423d2971377919b49591ca2c682323622
Reinaldo Bruno <reinaldo_estiva@hotmail.com>
2024-03-05 03:01:43 -0300

    resposividade foi finalizada

--- scripts.js
+++ scripts.js
@@ -1,17 +1,50 @@
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+
+const menuHamburguer = document.querySelector(".menu-hamburguer")
+const nav = document.querySelector(".navbar")
+const links = document.querySelectorAll(".navbar-links li a");
+
+let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
+function createSwiper(container, pagination, nextButton, prevButton) {
+  return new Swiper(container, {
+    slidesPerView: handleWidth(),
     spaceBetween: 30,
     pagination: {
-      el: ".swiper-pagination",
+      el: pagination,
       clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+      nextEl: nextButton,
+      prevEl: prevButton,
+    },
   });
-
+}
 let header = document.getElementById("header")
+function handleWidth(){
+    let getWidth = window.innerWidth || document.documentElement.clientWidth;
+    let slideShow = 3;
+
+    if (getWidth < 1001){
+        slideShow = 2;
+    }
+    if (getWidth < 700){
+        slideShow = 1;
+    }
+    return slideShow
+}
+
+menuHamburguer.addEventListener("click",() => {
+    nav.classList.toggle("active");
+})
+links.forEach(item =>{
+    item.addEventListener("click",()=>{
+        nav.classList.toggle("active")
+    })
+})
+
+window.addEventListener("resize",() => {
+    swiper.params.slidesPerView = handleWidth();
+    swiper.update()
+})
 
 window.addEventListener("scroll",() => {
     if(window.scrollY >= 100){
